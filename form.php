3d163d0918503de9d03a781721dfd7f0b5746995
nauraizza <30435089+nauraizza@users.noreply.github.com>
2017-10-14 13:34:54 +0700

    Create form.php

---
+++ form.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Instagram</title>
+	<link rel="stylesheet" href="styleku.css">
+</head>
+<body>
+
+<div id ="container"> 
+
+	<div class="Header">
+		Instagram
+	</div>
+
+	<div class="subheader">
+		Sign up to see photos and videos <br>
+		from your firends.
+	</div>
+
+	<div class="button">
+		<form action="#" method="GET" >
+		<input type="submit" value="Login with Facebook">
+		</form>
+	</div>
+
+	<!--Punya Naura Izza-->
+	<!--Punya Naura Izza-->
+	<!--Punya Naura Izza-->
+
+	<div class="content">
+		<form action="hasil.php" method="GET">
+			<input type="email" name="email" placeholder="Mobile Number or Email"><br>
+			<input type="text"	name="fullname" placeholder="Full name"><br>
+			<input type="text"	name="user" placeholder="Username"><br>
+			<input type="password"	name="pw" placeholder="Password"><br>
+			<input type="submit" value="Sign Up">
+		</form>
+
+		
+	</div>
+
+		<div class="Footer">
+		By Signing Up, you agree to our <br>
+		Terms & Privacy Policy
+		</div>
+	
+</div>
+	
+
+
+</body>
+</html>
+
